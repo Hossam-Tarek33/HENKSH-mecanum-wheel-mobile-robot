288d629594b72a647029e5e8c2d0a5df7bb3b202
Hossam Abdelaziz <68400719+Hossam-Tarek33@users.noreply.github.com>
2021-12-17 12:13:46 +0100

    Add files via upload

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/hossam/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/hossam/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/hossam/catkin_ws/devel;/opt/ros/melodic' != '' else []

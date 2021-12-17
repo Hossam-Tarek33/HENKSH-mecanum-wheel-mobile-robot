621194e09bddb7032d09d9e8e04f38fdb7325243
Hossam Abdelaziz <68400719+Hossam-Tarek33@users.noreply.github.com>
2021-12-17 12:20:35 +0100

    cmake

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/hossam/catkin_ws/src
+# Build directory: /home/hossam/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("henksh_control_1_motor")
+subdirs("henksh_control_keyboard")
+subdirs("henksh_encoders")
